ff528106dea5975b59a2d533e9f1fbce3615d3cc
a0z9 <a0z9@rambler.ru>
2019-02-08 18:13:03 +0300

    3

--- src/main/java/org/andestech/learning/rfb19/g3/AppJAXB.java
+++ src/main/java/org/andestech/learning/rfb19/g3/AppJAXB.java
@@ -3,8 +3,11 @@ package org.andestech.learning.rfb19.g3;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.jar.JarException;
 
 public class AppJAXB {
@@ -14,6 +17,21 @@ public class AppJAXB {
         Book book =
                 new Book(1869,"GG-864876","Лев Толстой","Война и Мир");
 
+        Book book2 =
+                new Book(1889,"GT-823464876","Лев Толстой","Крейцерова соната");
+
+
+        Library library = new Library();
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(book);
+        books.add(book2);
+        library.setBookList(books);
+
+
+
+
+        Book bookFromXml = null;
+
       try {
 
           JAXBContext ctx = JAXBContext.newInstance(Book.class);
@@ -21,7 +39,28 @@ public class AppJAXB {
           Marshaller marshaller = ctx.createMarshaller();
           marshaller.setProperty("jaxb.formatted.output", true);
 
-          marshaller.marshal(book, new FileOutputStream("e:\\datas3\\book1.xml"));
+          marshaller.marshal(book, new FileOutputStream(
+                  "e:\\datas3\\book1.xml"));
+
+          //--------------------------
+
+          Unmarshaller unmarshaller = ctx.createUnmarshaller();
+
+          bookFromXml = (Book) unmarshaller.unmarshal(
+                  new FileInputStream("e:\\datas3\\book1.xml"));
+
+          System.out.println(bookFromXml);
+
+
+          //------------------------------------------------------------
+
+          JAXBContext ctx2 = JAXBContext.newInstance(Library.class);
+
+          Marshaller marshaller2 = ctx2.createMarshaller();
+          marshaller2.setProperty("jaxb.formatted.output", true);
+
+          marshaller2.marshal(library, new FileOutputStream(
+                  "e:\\datas3\\library1.xml"));
 
       }
       catch (JAXBException | IOException ex){ex.printStackTrace();}
